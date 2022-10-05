3b293046bbfb345617c7e739cd64cb639bb843aa
aktarshasha <aktarshasha4@gmail.com>
2022-10-05 18:42:59 +0530

    print

---
+++ Day 1/debug.py
@@ -0,0 +1,11 @@
+print("and the function"+ "should")
+print ("hey\nhey")
+
+
+
+
+#correct the following lines
+print ("Day 1 - String Manipulation")
+print ('String Concatenation is done with the "+" sign.')
+print ('e.g. print("Hello " + "world")')
+print ("New lines can be created with a backslash and n.")
