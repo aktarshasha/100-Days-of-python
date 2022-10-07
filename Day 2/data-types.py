f42cf0914f00808e0e9b4be7b60c7a6c299ab9c0
aktarshasha <aktarshasha4@gmail.com>
2022-10-07 20:59:03 +0530

    Day2

--- Day 2/data-types.py
+++ Day 2/data-types.py
@@ -1,2 +1,6 @@
 #testing
-print ("hello"[4])
+two_digit_num = input("write a two digit number ")
+first_number =int (two_digit_num[0])
+second_number =int (two_digit_num[1])
+two_digit_num = first_number + second_number
+print (two_digit_num)
