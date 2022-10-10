224a99ed6d919568be2d9b026e028a716fa07ff6
aktarshasha <aktarshasha4@gmail.com>
2022-10-10 23:43:30 +0530

    Day3

---
+++ Day 3/roller coster 2.py
@@ -0,0 +1,17 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height >= 120 :
+ age = int(input("What is your age? "))
+ print("you can ride")
+
+ if age >= 18 :
+  print("you have to pay $12")
+ elif
+print("you have to pay")
+ else:
+    print("you have to pay $7")
+
+else:
+ print("you can't ride")
+ #think
+ 
