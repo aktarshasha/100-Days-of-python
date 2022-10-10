5ff35cb42b8bfe3fa17bebe967d939db51d6174f
aktarshasha <aktarshasha4@gmail.com>
2022-10-11 00:13:06 +0530

    Day3

---
+++ Day 3/suhail.py
@@ -0,0 +1,13 @@
+Your_hight = int(input("What is Your hight:- "))
+if Your_hight >= 120:
+    print("You are good to go to the roller coster")
+    your_age = int(input("What is your Age:- "))
+      if your_age > 8:
+        print("This is your ticket and have your seat on the roller coster")
+      else:
+        print("Sorry you can't go to the roler coster")
+#reyy sunnn ye complete hai code?
+yessss ek aur aata na else
+aur else is optionall
+wo if ka
+else kaha hai
