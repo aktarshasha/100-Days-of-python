396cb48d22eabf6a67f7f87997c94255bf5c0d79
aktarshasha <aktarshasha4@gmail.com>
2022-10-15 23:55:27 +0530

    Day3

--- Day 3/0dd,even, divisiblity.py
+++ Day 3/0dd,even, divisiblity.py
@@ -1,7 +1,8 @@
 #This is the programme to check odd even or any divisiblity
-number = int(input("Which number do you want to check? "))
-#put divisible no here
-if number %2 == 0:
+n = int(input("enter no."))
+
+
+if not (n and 1):
     print("This is an even number.")
 else:
     print("This is an odd number.")
---
+++ Day 3/test2.py
@@ -0,0 +1,6 @@
+y = int(input())
+if (y % 400 == 0) or (y % 100 != 0 and y %4 == 0):
+    print(y,"is leap year.")
+else:
+    print(y,"isn't leap year.")
+#commit
