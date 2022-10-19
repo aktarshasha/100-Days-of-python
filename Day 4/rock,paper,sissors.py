0da80f992b2eeee9653ea8b52a57bdae94e1fd23
aktarshasha <aktarshasha4@gmail.com>
2022-10-19 20:03:01 +0530

    Day4

--- Day 4/rock,paper,sissors.py
+++ Day 4/rock,paper,sissors.py
@@ -30,10 +30,40 @@ scissors = '''
 
 choice=input("What do you choose? Type 0 for Rock,1 for paper,2 for scissors ")
 
-if choice==0:
+print("you choose")
+if choice == '0':
  print(rock)
-elif choice==1:
+elif choice == '1':
  print(paper)
-elif choice==2:
+elif choice == '2':
  print(scissors)
-#halfdone 
+
+print("computer choose")
+
+import random
+c_choice= random.randint(0, 2)
+
+if c_choice == 0:
+ print(rock)
+elif c_choice == 1:
+ print(paper)
+elif c_choice == 2:
+ print(scissors)
+
+if choice<'0'or choice>='3':
+    print("invalid input")
+elif choice=='0' and c_choice==2:
+    print("you win!")
+elif choice=='1' and c_choice==0:
+    print("you win!")
+elif choice=='2' and c_choice==1:
+    print("you win!")
+elif choice=='2' and c_choice==0:
+    print("you loose!")
+elif choice=='0' and c_choice==1:
+    print("you loose!")
+elif choice=='1' and c_choice==2:
+    print("you loose!")
+elif choice==c_choice:
+    print("its a draw")
+    
